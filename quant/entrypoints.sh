22eeb4fc9988776830495f61c7bb4e9fbce60501
Stuart Rowlands <stuart.rowlands@quantcdn.io>
2025-08-11 15:12:08 +1000

    Redirect to stderr

--- quant/entrypoints.sh
+++ quant/entrypoints.sh
@@ -2,21 +2,21 @@
 
 # Execute all scripts in /quant/entrypoints/ then switch to www-data user
 
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] Running Quant entrypoints as root..."
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] Running Quant entrypoints as root..." >&2
 
 if [ -d /quant/entrypoints ]; then
   for i in /quant/entrypoints/*; do
     if [ -r $i ]; then
-      echo "[$(date +'%Y-%m-%d %H:%M:%S')] Executing entrypoint: $(basename $i)"
+      echo "[$(date +'%Y-%m-%d %H:%M:%S')] Executing entrypoint: $(basename $i)" >&2
       . $i
     fi
   done
   unset i
-  echo "[$(date +'%Y-%m-%d %H:%M:%S')] Quant entrypoints complete"
+  echo "[$(date +'%Y-%m-%d %H:%M:%S')] Quant entrypoints complete" >&2
 else
-  echo "[$(date +'%Y-%m-%d %H:%M:%S')] No /quant/entrypoints directory found"
+  echo "[$(date +'%Y-%m-%d %H:%M:%S')] No /quant/entrypoints directory found" >&2
 fi
 
 # Switch to www-data user for the main application
-echo "[$(date +'%Y-%m-%d %H:%M:%S')] Switching to www-data user for application..."
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] Switching to www-data user for application..." >&2
 exec gosu www-data "$@"
